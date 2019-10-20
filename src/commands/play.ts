265debe12a0d4ae06c9599f61fc142e2e3c8af31
Joshua Ramsbottom <joshuaramsbottom@electrum.co.za>
2019-10-20 13:26:44 +0200

    Play command can search

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,17 +1,39 @@
 import { Command, CommandoClient, CommandMessage } from 'discord.js-commando';
+import youtubeSearch, { YouTubeSearchResults } from 'youtube-search';
+
+const ytOptions: youtubeSearch.YouTubeSearchOptions = {
+  maxResults: 1,
+  key: process.env.YOUTUBE_API_KEY,
+};
 
 class PlayCommand extends Command {
+
   constructor(client: CommandoClient) {
     super(client, {
       name: 'play',
       group: 'music',
       memberName: 'play',
       description: 'Search YouTube and play a track',
-    })
+      guildOnly: true,
+      args: [
+        {
+          key: 'searchQuery',
+          prompt: 'What track would you like to search for?',
+          type: 'string',
+        },
+      ],
+    });
   }
 
-  run(message: CommandMessage) {
-    return message.say('Not ready yet!');
+  async run(message: CommandMessage, { searchQuery }: { searchQuery: string }) {
+    // Get top result from YouTube data API
+    const { results } = await youtubeSearch(searchQuery, ytOptions);
+    const result: YouTubeSearchResults = results[0];
+    const { link, title } = result;
+
+    // Add track to queue
+
+    return message.say(`Playing ${title}`);
   }
 }
 
