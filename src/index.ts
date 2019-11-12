9d22579f05829fe3ed7c0de7d6f226bb7154c97f
Joshua Ramsbottom <joshuaramsbottom@electrum.co.za>
2019-11-12 23:13:05 +0200

    Don't use pino loggers as functions directly

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ client.once('ready', () => {
   logger.info(`Logged in as ${client.user.tag} (${client.user.id})`);
   client.user.setActivity('!help');
 });
-client.on('debug', logger.debug);
-client.on('warn', logger.warn);
-client.on('error', logger.error);
+client.on('debug', message => logger.debug(message));
+client.on('warn', message => logger.warn(message));
+client.on('error', message => logger.error(message));
 
 process.on('uncaughtException', pino.final(logger, (error, finalLogger) => {
   finalLogger.error(error, 'uncaughtException');
